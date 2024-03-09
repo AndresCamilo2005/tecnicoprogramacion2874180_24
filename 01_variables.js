7ff82724b6f7516fae9e6b158bafe3d5e7d82f20
andrescamilo2005 <andreschito092@gmail.com>
2024-03-09 11:17:18 -0500

    respaldo 1

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+let saludo = "hola mundo";
+console.log(saludo);
+
+let nombres, direcion, telefono;
+nombres= "andres camilo";
+direcion= "calle 10 19-34";
+telefono =3117217443 ;
+let salario =100000;
+var profesion ="aprendis";
+console.log(nombres,direcion,telefono,salario,profesion)
+
+
+
+document.write(nombres,direcion,telefono,salario,profesion + "<br>")
